792e2efd3c544d5a70edcc0d9865f53f3d85fa95
김진영 <aiqextory@gmail.com>
2025-02-02 16:58:58 +0900

    fix: 누락되었던 shared/ui 컴포넌트 인터페이스 복구

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,4 +3,6 @@ export { Button, buttonVariants } from './button';
 export { Checkbox } from './checkbox';
 export { Icons } from './icons';
 export { Input } from './input';
+export { Label } from './label';
+export { RadioGroup, RadioGroupItem } from './radio-group';
 export { Separator } from './separator';
