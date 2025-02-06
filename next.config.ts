e8b73e186c9cc57ba96ab83b766fe6298432340b
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2025-02-06 23:41:06 +0900

    fix: https => http api 요청 Mixed Contents Error(임시 방편2): rewrite

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://your-api-server.com/api/:path*',
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
