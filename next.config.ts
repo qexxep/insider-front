1955005405836d6d9966b384fbae6bb7a53802c6
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2025-02-09 14:13:31 +0900

    chore: svgr 라이브러리 설치 및 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,5 @@
+import type { Configuration } from 'webpack';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
@@ -24,6 +26,24 @@ const nextConfig = {
         ],
       },
     ];
+  },
+  webpack: (config: Configuration) => {
+    config.module?.rules?.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
   },
 };
 
