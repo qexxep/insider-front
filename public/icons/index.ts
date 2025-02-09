c1748054e5310b086b8bdb73ac244a2c0f9e7c5e
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2025-02-09 14:14:38 +0900

    chore: svg 추가(card_line,first,second,third)

---
+++ public/icons/index.ts
@@ -0,0 +1,6 @@
+import RankingLine from './card_line.svg';
+import First from './first.svg';
+import Second from './second.svg';
+import Third from './third.svg';
+
+export { First, RankingLine, Second, Third };
