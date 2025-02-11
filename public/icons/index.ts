e2ee902b62b4a2e27f16565fabf25560bea2d326
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2025-02-11 23:35:52 +0900

    chore: loading svg 추가

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,6 +1,7 @@
 import RankingLine from './card_line.svg';
 import First from './first.svg';
+import Loading from './loading.svg';
 import Second from './second.svg';
 import Third from './third.svg';
 
-export { First, RankingLine, Second, Third };
+export { First, Loading, RankingLine, Second, Third };
