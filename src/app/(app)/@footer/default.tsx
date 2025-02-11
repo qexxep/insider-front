746a44dd1f87921d6e3aec6bbb0384cee8967e1f
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2025-02-11 23:41:13 +0900

    refactor: footer 일부 수정

--- src/app/(app)/@footer/default.tsx
+++ src/app/(app)/@footer/default.tsx
@@ -1,19 +1,24 @@
+import { Separator } from '@/shared/ui';
+
 export default function Footer() {
   return (
     <footer className="min-h-[120px] pt-5">
       <div className="mx-auto max-w-7xl px-4">
-        <div className="flex justify-center space-x-4 text-sm text-gray-600">
+        <div className="flex items-center justify-center space-x-4 text-sm text-gray-600">
+          {/* Link로 교체될 예정 */}
           <span>회사소개</span>
-          <span className="text-gray-300">|</span>
+          <Separator orientation="vertical" className="h-4" />
           <span>제휴안내</span>
-          <span className="text-gray-300">|</span>
+          <Separator orientation="vertical" className="h-4" />
           <span>광고안내</span>
-          <span className="text-gray-300">|</span>
+          <Separator orientation="vertical" className="h-4" />
           <span>이용약관</span>
-          <span className="text-gray-300">|</span>
+          <Separator orientation="vertical" className="h-4" />
           <span>개인정보처리방침</span>
         </div>
-        <div className="mt-5 text-center text-sm text-black">Copyright © 2024 INSIDER. All rights reserved.</div>
+        <small className="mt-5 block text-center text-sm text-black">
+          Copyright © 2024 INSIDER. All rights reserved.
+        </small>
       </div>
     </footer>
   );
