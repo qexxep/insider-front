7c687a1ffc567f815fb1ac64ba7e4f2942fbd0c9
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2025-02-12 01:32:54 +0900

    refactor: server component 사용을 위한 일부 조치

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig = {
       {
         source: '/api/:path*',
         destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+        basePath: false,
       },
     ];
   },
--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -3,17 +3,19 @@ import ky, { BeforeRequestHook } from 'ky-universal';
 import { getLocalStorage } from '../utils';
 
 const setAuthorizationHeader: BeforeRequestHook = request => {
-  const accessToken = getLocalStorage('accessToken');
-  if (accessToken) {
-    request.headers.set('Authorization', `Bearer ${accessToken}`);
+  // 서버에서 실행되지 않기 위한 조건 추가
+  if (typeof window !== 'undefined') {
+    const accessToken = getLocalStorage('accessToken');
+    if (accessToken) {
+      request.headers.set('Authorization', `Bearer ${accessToken}`);
+    }
   }
 };
 
-const baseApiClient = ky.create({
-  // prefixUrl: 'http://inssider.kro.kr/api',
-});
+const baseUrl = typeof window === 'undefined' ? process.env.NEXT_PUBLIC_BASE_URL : '/';
 
-const apiClient = baseApiClient.extend({
+const apiClient = ky.create({
+  prefixUrl: baseUrl,
   hooks: {
     beforeRequest: [setAuthorizationHeader],
   },
