2062403ab9e86c860944f0b33b351793033ea5be
김진영 <aiqextory@gmail.com>
2025-02-16 14:28:22 +0900

    edit: 회원가입 폼 스키마 수정

--- src/views/sign-in/model.ts
+++ src/views/sign-in/model.ts
@@ -9,30 +9,34 @@ export type LoginFormType = z.infer<typeof LoginFormSchema>;
 
 export const SignupFormSchema = z
   .object({
-    nickName: z.string().min(2, {
-      message: 'Username must be at least 2 characters.',
-    }),
+    nickName: z
+      .string()
+      .min(4, { message: '닉네임은 최소 4자 이상이어야 합니다.' })
+      .max(12, { message: '닉네임은 최대 12자 이하여야 합니다.' }),
     userId: z
       .string()
-      .min(3, { message: 'User ID must be at least 3 characters.' })
-      .max(20, { message: 'User ID must be at most 20 characters.' }),
+      .min(4, { message: '아이디는 최소 4자 이상이어야 합니다.' })
+      .max(20, { message: '아이디는 최대 12자 이하여야 합니다.' }),
     password: z
       .string()
-      .min(6, { message: 'Password must be at least 6 characters.' })
-      .max(30, { message: 'Password must be at most 30 characters.' })
-      .regex(/[A-Z]/, { message: 'Password must contain at least one uppercase letter.' }) // 대문자 요구 사항 추가
-      .regex(/[a-z]/, { message: 'Password must contain at least one lowercase letter.' }) // 소문자 요구 사항 추가
-      .regex(/[0-9]/, { message: 'Password must contain at least one number.' }), // 숫자 요구 사항 추가
+      .min(8, { message: '비밀번호는 최소 8자 이상이어야 합니다.' })
+      .max(20, { message: '비밀번호는 최대 20자 이하여야 합니다.' })
+      .regex(/[A-Za-z]/, { message: '비밀번호에는 최소 한 개의 영문자가 포함되어야 합니다.' })
+      .regex(/[0-9]/, { message: '비밀번호에는 최소 한 개의 숫자가 포함되어야 합니다.' })
+      .regex(/[!@#$%^&*()]/, { message: '비밀번호에는 최소 한 개의 특수문자(!@#$%^&*())가 포함되어야 합니다.' }),
     confirmPassword: z.string(),
-    email: z.string().email({ message: 'Please enter a valid email address.' }), // 이메일 유효성 검사
-    tempCode: z.string(),
+    email: z.string().email({ message: '이메일 형식에 맞지 않습니다.' }),
+    tempCode: z
+      .string()
+      .length(6, { message: '인증번호는 6자리 숫자여야 합니다.' })
+      .regex(/^\d{6}$/, { message: '인증번호는 숫자 6자리여야 합니다.' }),
     birthDate: z.string(),
     gender: z.enum(['M', 'F', 'other'], {
-      required_error: 'You need to select a notification type.',
+      required_error: '성별을 선택해 주세요.',
     }),
   })
   .refine(data => data.password === data.confirmPassword, {
-    message: "Passwords don't match",
+    message: '비밀번호가 일치하지 않습니다.',
     path: ['confirmPassword'],
   });
 
