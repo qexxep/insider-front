cf850ca5ba0ab76b59267532108eea5c2c29c934
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2025-02-18 00:01:25 +0900

    style: header, sidebar 스타일 수정

--- src/app/(app)/@header/default.tsx
+++ src/app/(app)/@header/default.tsx
@@ -20,24 +20,24 @@ export default async function Header() {
   const token = cookieStore.get('access_token')?.value;
 
   return (
-    <header className="sticky top-0 z-50 h-14 w-full border-b border-border/40 bg-background/95 shadow-sm backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="supports-[backdrop-filter]:bg-background/6 0 sticky top-0 z-50 h-14 w-full border-b border-border/40 bg-background/95 shadow-md backdrop-blur">
       <div className="flex h-full w-full items-center justify-between px-6">
         {/* 좌측 */}
         <div className="flex items-center gap-24">
           <Link href="/" className="flex items-center gap-2">
-            <Icons.logo className="h-11 w-11" />
+            <Icons.logo className="h-10 w-10" />
             <span className="text-2xl font-extrabold text-primary">INSIDER</span>
           </Link>
           <HotTopics />
         </div>
         {/* 중앙 */}
-        <div className="flex max-w-sm flex-1 items-center justify-center px-4">
+        <div className="absolute left-1/2 top-1/2 w-full max-w-[557px] -translate-x-1/2 -translate-y-1/2">
           <div className="relative flex h-full w-full items-center">
             <Icons.search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-primary" />
             <Input
               type="search"
               placeholder="검색어를 입력하세요."
-              className="h-8 rounded-full border-primary pl-10 focus:border-2 focus-visible:ring-0 focus-visible:ring-offset-0"
+              className="h-10 w-full rounded-full border-primary pl-10 focus:border-2 focus-visible:ring-0 focus-visible:ring-offset-0"
             />
           </div>
         </div>
@@ -65,9 +65,14 @@ export default async function Header() {
         )}
         {!token && (
           <div className="flex items-center gap-3">
-            <Link href={'/signup'}>회원가입</Link>
+            <Link href={'/signup'} className="transition-colors hover:font-bold hover:text-foreground">
+              회원가입
+            </Link>
             <Separator orientation="vertical" className="h-3" />
-            <Link href={'/login'} className="text-primary">
+            <Link
+              href={'/login'}
+              className="text-primary text-opacity-80 transition-colors hover:font-bold hover:text-primary"
+            >
               로그인
             </Link>
           </div>
@@ -81,27 +86,28 @@ async function HotTopics() {
   const { data: topics } = await getHotTopics();
 
   return (
-    <div className="flex min-w-[300px] items-center justify-between">
+    <div className="flex items-center justify-between gap-10">
       <div className="flex items-center gap-3 overflow-hidden px-1 py-1">
         <div className="flex items-center gap-1">
           <span className="text-sm font-medium text-primary">HOT</span>
           <span className="text-sm font-medium text-gray-500">토픽</span>
         </div>
         <div className="relative h-7 overflow-hidden">
-          <div
-            className="flex flex-col"
+          <ul
+            className="flex flex-col transition-all duration-500"
             style={{
-              animation: topics && topics.length >= 2 ? 'smoothCarousel 30s steps(10, end) infinite' : 'none',
+              animation: topics && topics.length >= 2 ? 'rollingAnimation 25s linear infinite' : 'none',
             }}
           >
             {topics && topics.length > 0
-              ? [...topics, ...topics].slice(0, 10).map((topic, index) => (
-                  <div key={`${topic.topicSeq}-${index}`} className="flex h-7 items-center gap-2">
+              ? [...topics, ...topics].map((topic, index) => (
+                  <li key={`${topic.topicSeq}-${index}`} className="relative flex h-7 min-w-[200px] items-center gap-2">
                     <span className="ml-1 flex h-5 w-5 items-center justify-center rounded-sm bg-gray-200 text-xs">
                       {topic.rankNum}
                     </span>
                     <span className="whitespace-nowrap text-sm">{topic.searchWord}</span>
-                  </div>
+                    <Icons.arrowBigUp className="absolute right-0 ml-5 h-4 w-4 justify-self-end fill-primary text-primary" />
+                  </li>
                 ))
               : Array.from({ length: 10 }, (_, i) => (
                   <div key={i} className="flex h-7 items-center gap-2">
@@ -111,10 +117,9 @@ async function HotTopics() {
                     <span className="whitespace-nowrap text-sm">로딩중...</span>
                   </div>
                 ))}
-          </div>
+          </ul>
         </div>
       </div>
-      <Icons.arrowBigUp className="h-4 w-4 fill-primary text-primary" />
     </div>
   );
 }
