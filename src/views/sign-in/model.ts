f5409bfffcf0e25e37ccee0fb157d95ef42ca498
김진영 <aiqextory@gmail.com>
2025-02-22 14:57:50 +0900

    edit: 회원가입 관련 유효성 스키마 변경

--- src/views/sign-in/model.ts
+++ src/views/sign-in/model.ts
@@ -18,12 +18,13 @@ export const SignupFormSchema = z
     nickName: z
       .string()
       .min(4, { message: '닉네임은 최소 4자 이상이어야 합니다.' })
-      .max(12, { message: '닉네임은 최대 12자 이하여야 합니다.' }),
+      .max(12, { message: '닉네임은 최대 12자 이하여야 합니다.' })
+      .regex(/^[a-zA-Z0-9가-힣]+$/, { message: '닉네임은 영문, 숫자, 한글만 사용할 수 있습니다.' }),
     userId: z
       .string()
       .min(4, { message: '아이디는 최소 4자 이상이어야 합니다.' })
-      .max(20, { message: '아이디는 최대 12자 이하여야 합니다.' })
-      .regex(/^[A-Za-z0-9]+$/, { message: '아이디는 영문자와 숫자만 포함되어야 합니다.' })
+      .max(20, { message: '아이디는 최대 20자 이하여야 합니다.' })
+      .regex(/^[A-Za-z0-9]+$/, { message: '아이디는 영문자와 숫자만 사용할 수 있습니다.' })
       .regex(/[A-Za-z]/, { message: '아이디에는 최소 한 개의 영문자가 포함되어야 합니다.' }),
     password: z
       .string()
@@ -35,7 +36,6 @@ export const SignupFormSchema = z
     confirmPassword: z.string(),
     email: z.string().email({ message: '이메일 형식에 맞지 않습니다.' }),
     tempCode: tempCodeSchema,
-    // birthDate: z.string(),
     'birthDate-year': z.string().length(4),
     'birthDate-month': z.string().min(1).max(2),
     'birthDate-day': z.string().min(1).max(2),
