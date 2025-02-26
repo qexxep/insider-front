b4c5a28678916dda25f440f3e2e7fbf017be02e5
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2025-02-26 23:45:19 +0900

    style: 컬러 디자인토큰 변경

--- src/views/posts/ui/WritePostPage.tsx
+++ src/views/posts/ui/WritePostPage.tsx
@@ -63,17 +63,17 @@ export function WritePostPage() {
         </Card>
 
         {/* 첨부한 이미지 미리보기 */}
-        <div className="flex flex-wrap gap-2 rounded-md bg-[#f8f8f8] p-6">
-          <div className="h-24 w-24 rounded-md bg-gray-200"></div>
-          <div className="h-24 w-24 rounded-md bg-gray-200"></div>
-          <div className="h-24 w-24 rounded-md bg-gray-200"></div>
-          <div className="h-24 w-24 rounded-md bg-gray-200"></div>
-          <div className="h-24 w-24 rounded-md bg-gray-200"></div>
-          <div className="h-24 w-24 rounded-md bg-gray-200"></div>
-          <div className="h-24 w-24 rounded-md bg-gray-200"></div>
-          <div className="h-24 w-24 rounded-md bg-gray-200"></div>
-          <div className="h-24 w-24 rounded-md bg-gray-200"></div>
-          <div className="h-24 w-24 rounded-md bg-gray-200"></div>
+        <div className="flex flex-wrap gap-2 rounded-md bg-gray-100 p-6">
+          <div className="h-24 w-24 rounded-md bg-gray-400"></div>
+          <div className="h-24 w-24 rounded-md bg-gray-400"></div>
+          <div className="h-24 w-24 rounded-md bg-gray-400"></div>
+          <div className="h-24 w-24 rounded-md bg-gray-400"></div>
+          <div className="h-24 w-24 rounded-md bg-gray-400"></div>
+          <div className="h-24 w-24 rounded-md bg-gray-400"></div>
+          <div className="h-24 w-24 rounded-md bg-gray-400"></div>
+          <div className="h-24 w-24 rounded-md bg-gray-400"></div>
+          <div className="h-24 w-24 rounded-md bg-gray-400"></div>
+          <div className="h-24 w-24 rounded-md bg-gray-400"></div>
         </div>
       </div>
     </div>
