409adb122a117694a7c54ea51ca4d8db74939c69
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2025-03-04 18:48:21 +0900

    chore: query config 엔트리에 추가

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -4,6 +4,7 @@ export type { ApiResponse } from './types';
 
 // Query 관련 export
 export { createQueryClient } from './query/client';
+export { DEFAULT_MUTATION_OPTIONS, DEFAULT_QUERY_OPTIONS, ENV_SPECIFIC_OPTIONS } from './query/config';
 export { QUERY_KEYS } from './query/keys';
 export { useCommonMutation } from './query/mutation';
 export { useCommonQuery } from './query/query';
