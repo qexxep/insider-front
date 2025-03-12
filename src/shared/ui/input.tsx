54d8b650e573c7beff632e378d7c3cf81dfb2d00
김진영 <aiqextory@gmail.com>
2025-03-12 22:45:47 +0900

    feat: 공통 Input 스타일링 시스템 반영 맟 로그인 페이지 스타일 적용

--- src/shared/ui/input.tsx
+++ src/shared/ui/input.tsx
@@ -1,22 +1,34 @@
+import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
-import { cn } from '@/shared/lib/tw-utils';
+import { cn } from '../lib';
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          'flex h-12 w-full rounded-md border border-input bg-background px-3 py-2 text-[16px] text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50',
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
+const inputVariants = cva(
+  cn(
+    'flex h-12 w-full rounded-[6px] border bg-background px-3 py-2 text-[16px] text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground',
+    'focus-visible:border-primary focus-visible:bg-[#FFEEE4] focus-visible:text-[#242424] focus-visible:outline-none focus-visible:ring-0',
+    'disabled:cursor-not-allowed disabled:opacity-50'
+  ),
+  {
+    variants: {
+      status: {
+        default: 'bg-background border-[#8F8F8F]',
+        success: 'border-input-success text-input-success',
+        error: 'border-input-error text-input-error',
+        warning: 'border-input-warning text-input-warning',
+      },
+    },
+    defaultVariants: {
+      status: 'default',
+    },
   }
 );
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>, VariantProps<typeof inputVariants> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, status, type, ...props }, ref) => {
+  return <input type={type} className={cn(inputVariants({ status }), className)} ref={ref} {...props} />;
+});
 Input.displayName = 'Input';
 
 export { Input };
