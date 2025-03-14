b963b5f25550e150f6bda6cc0d9d18ca3a62520f
김진영 <aiqextory@gmail.com>
2025-03-14 23:27:15 +0900

    edit: 서버 컴포넌트 코드 제거

--- src/app/(app)/posts/[category]/[postId]/page.tsx
+++ src/app/(app)/posts/[category]/[postId]/page.tsx
@@ -1,8 +1,4 @@
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-
-import { commentPrefetchQueries } from '@/feature/comment';
-import { queryClient } from '@/shared/lib';
-import { DEFAULT_CURRENT_PAGE, DEFAULT_PAGE_SIZE, PostDetail, postPrefetchQueries } from '@/views/posts';
+import { PostDetail } from '@/views/posts';
 
 interface PageProps {
   params: Promise<{ category: string; postId: string }>;
@@ -13,27 +9,5 @@ export default async function Page({ params, searchParams }: PageProps) {
   const { postId, category } = await params;
   const { page } = await searchParams;
 
-  await Promise.all([
-    await postPrefetchQueries.detail(queryClient, { postSeq: postId }),
-    await commentPrefetchQueries.list(queryClient, {
-      postSeq: postId,
-      currPage: page ?? DEFAULT_CURRENT_PAGE,
-      pageSize: DEFAULT_PAGE_SIZE,
-      sortType: 'D',
-    }),
-    await postPrefetchQueries.bestWorst(queryClient, { categoryCd: category }),
-    await postPrefetchQueries.list(queryClient, {
-      categoryCd: category,
-      currPage: DEFAULT_CURRENT_PAGE,
-      pageSize: DEFAULT_PAGE_SIZE,
-    }),
-  ]);
-
-  const dehydratedState = dehydrate(queryClient);
-
-  return (
-    <HydrationBoundary state={dehydratedState}>
-      <PostDetail postId={postId} category={category} currentPage={page} />
-    </HydrationBoundary>
-  );
+  return <PostDetail postId={postId} category={category} currentPage={page} />;
 }
--- src/app/(app)/posts/[category]/page.tsx
+++ src/app/(app)/posts/[category]/page.tsx
@@ -1,7 +1,4 @@
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-
-import { queryClient } from '@/shared/lib';
-import { CategoryPostList, DEFAULT_CURRENT_PAGE, DEFAULT_PAGE_SIZE, postPrefetchQueries } from '@/views/posts';
+import { CategoryPostList } from '@/views/posts';
 
 interface PageProps {
   params: Promise<{ category: string }>;
@@ -9,21 +6,5 @@ interface PageProps {
 
 export default async function Page({ params }: PageProps) {
   const { category } = await params;
-
-  await Promise.all([
-    postPrefetchQueries.bestWorst(queryClient, { categoryCd: category }),
-    postPrefetchQueries.list(queryClient, {
-      categoryCd: category,
-      currPage: DEFAULT_CURRENT_PAGE,
-      pageSize: DEFAULT_PAGE_SIZE,
-    }),
-  ]);
-
-  const dehydratedState = dehydrate(queryClient);
-
-  return (
-    <HydrationBoundary state={dehydratedState}>
-      <CategoryPostList category={category} />
-    </HydrationBoundary>
-  );
+  return <CategoryPostList category={category} />;
 }
