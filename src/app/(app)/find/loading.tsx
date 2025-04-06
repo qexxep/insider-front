0e98cacb2bcea6677de04aa5ee0d0f5b23b18506
kelvin-chihyun <shclgus111@gmail.com>
2025-04-06 10:54:22 +0900

    style: loadingSpinner 위치 변경

---
+++ src/app/(app)/find/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingSpinner } from '@/shared/ui';
+
+export default function Loading() {
+  return <LoadingSpinner />;
+}
--- src/shared/ui/loading.tsx
+++ src/shared/ui/loading.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export const LoadingSpinner = () => {
   return (
-    <div className="flex h-full min-h-screen w-full flex-1 items-center justify-center pb-[25%] pr-[260px]">
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
       <Image src="/icons/loading.svg" alt="loading" width={200} height={200} priority />
     </div>
   );
