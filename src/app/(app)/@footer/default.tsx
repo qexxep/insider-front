f5a1a15c547176e635ce5136a69b5c274df8235c
kelvin-chihyun <shclgus111@gmail.com>
2025-04-14 20:14:32 +0900

    refactor: 불필요 요소 제거

--- src/app/(app)/@footer/default.tsx
+++ src/app/(app)/@footer/default.tsx
@@ -5,13 +5,6 @@ export default function Footer() {
     <footer className="min-h-[120px] pt-5">
       <div className="mx-auto max-w-7xl px-4">
         <div className="flex items-center justify-center space-x-4 text-sm text-gray-600">
-          {/* Link로 교체될 예정 */}
-          <span>회사소개</span>
-          <Separator orientation="vertical" className="h-4" />
-          <span>제휴안내</span>
-          <Separator orientation="vertical" className="h-4" />
-          <span>광고안내</span>
-          <Separator orientation="vertical" className="h-4" />
           <span>이용약관</span>
           <Separator orientation="vertical" className="h-4" />
           <span>개인정보처리방침</span>
